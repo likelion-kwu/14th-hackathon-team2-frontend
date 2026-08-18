190acd13e70a8ce4468e37605cd4106a05ceaa56
eine0322 <eine113589@gmail.com>
2026-08-18 17:50:37 +0900

    feat : svg mapping (#7)

---
+++ src/components/icon/IconMap.js
@@ -0,0 +1,25 @@
+// Navigation Bar Icons
+import navigationHomeActive from '../../assets/icons/icons-navigation/icons-navigation-home-active.svg?react'
+import navigationHomeDisabled from '../../assets/icons/icons-navigation/icons-navigation-home-disabled.svg?react'
+import navigationReflecAtive from '../../assets/icons/icons-navigation/icons-navigation-reflec-active.svg?react'
+import navigationReflecDisabled from '../../assets/icons/icons-navigation/icons-navigation-reflec-disabled.svg?react'
+import navigationRoutineActive from '../../assets/icons/icons-navigation/icons-navigation-routine-active.svg?react'
+import navigationRoutineDisabled from '../../assets/icons/icons-navigation/icons-navigation-routine-disabled.svg?react'
+import navigationRankActive from '../../assets/icons/icons-navigation/icons-navigation-rank-active.svg?react'
+import navigationRankDisabled from '../../assets/icons/icons-navigation/icons-navigation-rank-disabled.svg?react'
+import navigationCustomActive from '../../assets/icons/icons-navigation/icons-navigation-custom-active.svg?react'
+import navigationCustomDisabled from '../../assets/icons/icons-navigation/icons-navigation-custom-disabled.svg?react'
+
+export const iconMap = {
+  // Navigation Bar Icons
+  'navigation-home-active': navigationHomeActive,
+  'navigation-home-disabled': navigationHomeDisabled,
+  'navigation-reflec-active': navigationReflecAtive,
+  'navigation-reflec-disabled': navigationReflecDisabled,
+  'navigation-routine-active': navigationRoutineActive,
+  'navigation-routine-disabled': navigationRoutineDisabled,
+  'navigation-rank-active': navigationRankActive,
+  'navigation-rank-disabled': navigationRankDisabled,
+  'navigation-custom-active': navigationCustomActive,
+  'navigation-custom-disabled': navigationCustomDisabled,
+}
--- src/components/main-layout/navigation/Navigation.jsx
+++ src/components/main-layout/navigation/Navigation.jsx
@@ -1,5 +1,12 @@
+import { Icon } from '../../icon/Icon'
+
 function Navigation() {
-  return <div>Navigation</div>
+  return (
+    <div>
+      Navigation
+      <Icon name='navigation-home-active' width={24} height={24} />
+    </div>
+  )
 }
 
 export default Navigation
