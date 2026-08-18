de31ef11c629257260c680e7bc16e120d363ca2a
sunpine13 <sunpine2013@gmail.com>
2026-08-19 05:25:24 +0900

    feat : avatarsetting funcion(#85)

--- src/pages/avatarset-page/AvatarSettingPage.jsx
+++ src/pages/avatarset-page/AvatarSettingPage.jsx
@@ -1,17 +1,41 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import './AvatarSettingPage.css'
 
 import MainTitle from '../../components/initial-page-title/MainTitle'
 import SubTitle from '../../components/initial-page-title/SubTitle'
 import BackButton from '../../components/back-button/BackButton'
 import BottomButton from '../../components/bottom-button/BottomButton'
+import PopupPage from './popup-page/PopupPage'
 
 function AvatarSettingPage() {
+  const navigate = useNavigate()
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+  const handleBack = () => {
+    navigate('/tracksetting')
+  }
+
+  const handleOpenPopup = () => {
+    setIsPopupOpen(true)
+  }
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false)
+  }
+
+  const handleNext = () => {
+    navigate('/story')
+  }
+
   return (
     <div className='avatar-setting-page'>
-      <div className='avatar-setting-page__back-area'>
-        <BackButton />
-      </div>
       <div className='avatar-setting-page__scroll-area'>
+        <div className='avatar-setting-page__back-area'>
+          <BackButton onClick={handleBack} />
+        </div>
+
         <div className='avatar-setting-page__header'>
           <MainTitle>
             나만의 아바타를
@@ -26,7 +50,11 @@ function AvatarSettingPage() {
           <section className='avatar-setting-page__section'>
             <h2 className='avatar-setting-page__section-title'>아바타 생성</h2>
 
-            <button className='avatar-setting-page__generator' type='button'>
+            <button
+              className='avatar-setting-page__generator'
+              type='button'
+              onClick={handleOpenPopup}
+            >
               여기를 클릭해 아바타 생성하기
             </button>
           </section>
@@ -91,8 +119,10 @@ function AvatarSettingPage() {
           <span />
         </div>
 
-        <BottomButton disabled>다음</BottomButton>
+        <BottomButton onClick={handleNext}>다음</BottomButton>
       </footer>
+
+      {isPopupOpen && <PopupPage onClose={handleClosePopup} />}
     </div>
   )
 }
