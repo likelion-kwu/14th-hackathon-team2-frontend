8bb93792b00de26e41ae0eacf8edf7d9b02ef25b
sunpine13 <sunpine2013@gmail.com>
2026-08-19 05:30:47 +0900

    feat : popup ui(#86)

--- src/pages/avatarset-page/popup-page/PopupPage.jsx
+++ src/pages/avatarset-page/popup-page/PopupPage.jsx
@@ -1,5 +1,35 @@
-function PopupPage() {
-  return null
+import './PopupPage.css'
+
+function PopupPage({ onClose }) {
+  const handleSheetClick = (event) => {
+    event.stopPropagation()
+  }
+
+  return (
+    <div
+      className='popup-page'
+      role='dialog'
+      aria-modal='true'
+      aria-labelledby='avatar-popup-title'
+      onClick={onClose}
+    >
+      <section className='popup-page__sheet' onClick={handleSheetClick}>
+        <div className='popup-page__handle' aria-hidden='true' />
+
+        <h2 id='avatar-popup-title' className='popup-page__title'>
+          내 모습 촬영하기
+        </h2>
+
+        <p className='popup-page__description'>너무 어둡거나 밝은 곳, 조명이 아래는 피해 주세요</p>
+
+        <div className='popup-page__preview' />
+
+        <button className='popup-page__continue-button' type='button' onClick={onClose}>
+          이 모습으로 계속하기
+        </button>
+      </section>
+    </div>
+  )
 }
 
 export default PopupPage
