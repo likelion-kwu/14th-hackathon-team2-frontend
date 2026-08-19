00fa215d2d221a3bf1c5be8a5da2874311aac8a5
eine0322 <eine113589@gmail.com>
2026-08-19 13:08:32 +0900

    build :vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Filaby',
+        short_name: 'Filaby',
+        description: '루틴을 통해 또 다른 나와 함께 성장하는 웰니스 서비스',
+
+        start_url: '/',
+        display: 'standalone',
+
+        background_color: '#ffffff',
+        theme_color: '#4DC4FF',
+
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
