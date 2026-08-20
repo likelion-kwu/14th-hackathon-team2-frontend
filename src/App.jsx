f6310d1d8299288a9e7f79abc42887bff1a2ec7a
sunpine13 <sunpine2013@gmail.com>
2026-08-21 02:10:05 +0900

    feat : custompage popup(#190)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import { AppRouter } from './router/AppRouter'
 import styles from './App.module.css'
 
@@ -6,6 +7,38 @@ function App() {
   return (
     <div className={styles.app}>
       <RouterProvider router={AppRouter} />
+
+      <Toaster
+        position='bottom-center'
+        containerStyle={{
+          bottom: '100px',
+        }}
+        toastOptions={{
+          style: {
+            padding: '14px 20px',
+            borderRadius: '100px',
+            backgroundColor: 'rgba(45, 48, 52, 0.9)',
+            color: '#ffffff',
+            fontFamily: 'inherit',
+            fontSize: '14px',
+          },
+          loading: {
+            duration: Infinity,
+          },
+          success: {
+            duration: 2000,
+            style: {
+              backgroundColor: '#72c96b',
+            },
+          },
+          error: {
+            duration: 2500,
+            style: {
+              backgroundColor: '#e56868',
+            },
+          },
+        }}
+      />
     </div>
   )
 }
