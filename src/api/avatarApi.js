f7e7a90f2e08623aef07b32fb44f955956ad24f6
sunpine13 <sunpine2013@gmail.com>
2026-08-20 15:07:05 +0900

    feat : AvatarSettingPage api(#156)

---
+++ src/api/avatarApi.js
@@ -0,0 +1,16 @@
+import { apiRequest } from './apiClient'
+
+export function createAvatar({ growthTrack, facePhoto }) {
+  const formData = new FormData()
+
+  formData.append('growthTrack', growthTrack)
+
+  if (facePhoto) {
+    formData.append('facePhoto', facePhoto)
+  }
+
+  return apiRequest('/avatars/me', {
+    method: 'PUT',
+    body: formData,
+  })
+}
